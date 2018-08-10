6e3a9347b4711e7352546903e6f722a8006b88ca
Nehoko <fun4lsd@gmail.com>
2018-08-10 11:09:44 +0300

    I finally made it, but all in Main.class

---
+++ Palindrome/src/Palindrome.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+class Palindrome {
+
+    Palindrome (String text){
+        checkPalindrome(text);
+    }
+    private void checkPalindrome(String text) {
+        char[] chars = text.toCharArray();
+        try {
+            //Проверка количества символов
+            if (chars.length == 1 || chars.length > 500) {
+                throw new Exception("Неверное кол-во символов: введите текст с кол-вом символов от 2 до 500!");
+            }
+            //Проверка на наличие запрещённых символов
+            checkSymbols(text);
+
+            //Проверка слов на наличие полиндромов
+            wordPalindrome(text);
+
+            //Проверка предложений на наличие предложений-полиндромов
+            sentencePalindrome(text);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    //Проверка текста на запрещённые символы
+    private void checkSymbols(String text){
+        try{
+            if(Pattern.matches("[+\\-=%/`~^]",text)){
+                throw new Exception("Введен один или несколько запрещённых символов: ^ + = - * % / `");
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    //Проверка текста на слова-палиндромы
+    private void wordPalindrome(String text){
+        //Разбиение предложения на отдельные слова по пробелу
+        String[] strings = text.split("\\s");
+        for (int i = 0; i<strings.length; i++){
+            //Отделяем слово от ненужных знаков
+            String word = strings[i].replaceAll("\\p{Punct}","");
+            strings[i] = word;
+        }
+        ArrayList<String> palindromes = new ArrayList<>();
+        for (String string : strings) {
+            String words = string;
+            char[] chars = words.toCharArray();
+
+            //Цикл преобразования ЗАГЛАВНЫХ букв в строчные
+            for (int i = 0; i < chars.length; i++) {
+                char ch = chars[i];
+                chars[i] = Character.toLowerCase(ch);
+            }
+            words = new String(chars);
+
+            //Сама проверка слов
+            StringBuilder sb = new StringBuilder(words);
+            sb.reverse();
+            String sdrow = sb.toString();
+            if (words.equals(sdrow) && words.length() > 1) {
+                palindromes.add(string);
+            }
+        }
+        if (palindromes.size()>0)
+            System.out.println("Слова палиндромы:");
+        for (String palindrome : palindromes) {
+            System.out.println(palindrome);
+        }
+    }
+
+    //Проверка текста на предложения-палиндромы
+    private void sentencePalindrome(String text){
+        //Разделение предложений по точкам и пробелам после точки
+        String[] strings = text.split("\\.\\s");
+        ArrayList<String> palindromes = new ArrayList<>();
+        for (String string : strings) {
+            String sentence = string.replaceAll("[\\p{Punct}\\s]", "");
+            char[] chars = sentence.toCharArray();
+            //Цикл преобразования ЗАГЛАВНЫХ букв в строчные
+            for (int i = 0; i < chars.length - 1; i++) {
+                char ch = chars[i];
+                chars[i] = Character.toLowerCase(ch);
+            }
+            sentence = new String(chars);
+
+            //Сама проверка предложения
+            StringBuilder sb = new StringBuilder(sentence);
+            sb.reverse();
+            String ecnetnes = sb.toString();
+            if (sentence.equals(ecnetnes)) {
+                palindromes.add(string);
+            }
+        }
+        if (palindromes.size()>0)
+            System.out.println("Предложения палиндромы:");
+        for (String palindrome : palindromes){
+            System.out.println(palindrome);
+        }
+    }
+}
