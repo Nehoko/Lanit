1542e26f0cfe84c024e633213186d60d0144b905
Nehoko <fun4lsd@gmail.com>
2018-08-10 11:10:27 +0300

    I finally made it, and Palindrome as different Class.

--- Palindrome/src/Main.java
+++ Palindrome/src/Main.java
@@ -1,8 +1,5 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws Exception {
